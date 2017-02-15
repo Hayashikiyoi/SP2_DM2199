5ee3b92cd317633cbe9f4f1d84ac25985eb819ce
Lim Yi Chun <hayashikiyoi@gmail.com>
2017-02-15 10:51:18 +0800

    ->Update on Aloy_Scene for tga pics ->Implementing tga pic ->Implementing new text ->Implementing new FirstPersonCamera

---
+++ Application/Source/Fps_Camera.cpp
@@ -0,0 +1,100 @@
+#include "Fps_Camera.h"
+#include "Application.h"
+#include "Mtx44.h"
+#include "GLFW\glfw3.h"
+#include "MyMath.h"
+extern GLFWwindow* m_window;
+
+Fps_Camera::Fps_Camera()
+{
+}
+
+Fps_Camera::~Fps_Camera()
+{
+}
+
+void Fps_Camera::Init(const Vector3& pos, const Vector3& target, const Vector3& up)
+{
+	glfwSetInputMode(m_window, GLFW_CURSOR,GLFW_CURSOR_HIDDEN);//hide the cursor
+
+	this->position = defaultPosition = pos;
+	this->target = defaultTarget = target;
+	Vector3 view = (target - position).Normalized();
+	Vector3 right = view.Cross(up);
+	right.y = 0;
+	right.Normalize();
+	this->up = defaultUp = right.Cross(view).Normalized();
+}
+
+void Fps_Camera::Update(double dt)
+{
+	static const float CAMERA_SPEED = 50.f;
+	Vector3 view = (target - position).Normalized();
+	Vector3 right = view.Cross(up);
+	right.y = 0;
+	right.Normalize();
+	up = right.Cross(view).Normalized();
+	
+	//For movement
+	if (Application::IsKeyPressed('A'))
+	{
+		position -= right * (float)(CAMERA_SPEED * dt);
+		target = position + view * (float)(CAMERA_SPEED * dt);
+	}
+	if (Application::IsKeyPressed('D'))
+	{
+		position += right * (float)(CAMERA_SPEED * dt);
+		target = position + view * (float)(CAMERA_SPEED * dt);
+	}
+	if (Application::IsKeyPressed('W'))
+	{
+		position += view * (float)(CAMERA_SPEED * dt);
+		target = position + view * (float)(CAMERA_SPEED * dt);
+	}
+	if (Application::IsKeyPressed('S'))
+	{
+		position -= view * (float)(CAMERA_SPEED * dt);
+		target = position + view * (float)(CAMERA_SPEED * dt);
+	}
+	
+	glfwGetCursorPos(m_window, &xpos, &ypos);
+	
+	int mid_x = 800 / 2;
+	int mid_y = 600 / 2;
+
+	glfwSetCursorPos(m_window, mid_x, mid_y);
+
+	float horizMovement = Math::DegreeToRadian((float)(mid_x - xpos) * 3);
+	float vertMovement = Math::DegreeToRadian((float)(mid_y - ypos) * 3);
+
+	Mtx44 camYaw, camPitch;
+	Mtx44 rotation;
+
+	camYaw.SetToIdentity();
+	camPitch.SetToIdentity();
+
+	float angleofElevation = (view.y < 0 ? -1.0f : 1.0f)*Math::RadianToDegree(acos(Vector3(view.x, 0, view.z).Normalized().Dot(view)));
+
+	if (angleofElevation + vertMovement < 89.0f && angleofElevation + vertMovement>-89.0f)
+	{
+		camPitch.SetToRotation(vertMovement, right.x, right.y, right.z);
+	}
+	camYaw.SetToRotation(horizMovement, 0, 1, 0);
+	
+	rotation = camPitch*camYaw;
+	view = (rotation)*view;
+	target = position + view;
+	up = (rotation)* up;
+
+	if (Application::IsKeyPressed('R'))
+	{
+		Reset();
+	}
+}
+
+void Fps_Camera::Reset()
+{
+	position = defaultPosition;
+	target = defaultTarget;
+	up = defaultUp;
+}
