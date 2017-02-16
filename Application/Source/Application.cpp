019c00a2eb44d22e0666b95f34664b3ac5c41d74
Lim Yi Chun <hayashikiyoi@gmail.com>
2017-02-16 14:44:07 +0800

    Making a sub-version of Scene4 Code of Fps Camera again

--- Application/Source/Application.cpp
+++ Application/Source/Application.cpp
@@ -12,6 +12,8 @@
 
 #include "Scene_Calvert.h"
 #include "Aloy_Scene.h"
+#include "Scene4_Boss.h"
+#include "LoadScene.h"
 #include "ChuanXu.h"
 #include "Wilson.h"
 
@@ -109,8 +111,10 @@ void Application::Run()
 {
 	//Main Loop
 	SceneManager::instance()->addScene(new Aloy_Scene);
+	SceneManager::instance()->addScene(new LoadScene);
 	//SceneManager::instance()->addScene(new Wilson);
 	//SceneManager::instance()->addScene(new ChuanXu);
+	SceneManager::instance()->addScene(new Scene4_Boss);
 	SceneManager::instance()->addScene(new SceneUI); //Scene 0 (Add more scene like this way)
 	SceneManager::instance()->startScreen(); //Start of program (Initialise screen)
 
