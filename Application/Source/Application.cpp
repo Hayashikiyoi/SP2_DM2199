a47550e63431d8b668b860b7e17b69f2e3186901
Loo Chuan Xu <lchuanx@gmail.com>
2017-02-17 12:24:35 +0800

    Did up boss in Scene Chuan Xu, obj and tga in drive

--- Application/Source/Application.cpp
+++ Application/Source/Application.cpp
@@ -111,8 +111,9 @@ void Application::Init()
 void Application::Run()
 {
 	//Main Loop
-	SceneManager::instance()->addScene(new Aloy_Scene);
+	//SceneManager::instance()->addScene(new Aloy_Scene);
 	SceneManager::instance()->addScene(new Wilson);
+	SceneManager::instance()->addScene(new Scene1tutorial);
 	SceneManager::instance()->addScene(new ChuanXu);
 	//SceneManager::instance()->addScene(new SceneUI); //Scene 0 (Add more scene like this way)
 	SceneManager::instance()->startScreen(); //Start of program (Initialise screen)
